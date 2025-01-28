fc1526e1c5bf64523d6d6515d26597adab1987a2
82everywin <82everywin@gmail.com>
2025-01-29 04:59:02 +0900

    base-url 배포 사이트로 변경

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,10 @@
-export const API_BASE_URL = 'http://localhost:8080';
+//export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://bookmile.site';
 export const ACCESS_TOKEN = 'accessToken';
 export const REFRESH_TOKEN = 'refreshToken';
 
-export const OAUTH2_REDIRECT_URI = 'http://localhost:8080/login/oauth2/code/'
+//export const OAUTH2_REDIRECT_URI = 'http://localhost:8080/login/oauth2/code/'
+export const OAUTH2_REDIRECT_URI = 'https://bookmile.site/login/oauth2/code/';
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI+ 'google';
 export const KAKAO_AUTH_URL = API_BASE_URL + '/oauth2/authorize/kakao?redirect_uri=' + OAUTH2_REDIRECT_URI+'kakao';
